5f2e1fe5d01f7349740fa6bc2b23f1db7aeee05e
gouf <innocent.zero@gmail.com>
2018-08-05 21:00:13 +0900

    Update capistrano config for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,17 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+# frozen_string_literal: true
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# config valid for current version and patch releases of Capistrano
+lock '~> 3.11.0'
+
+set :application, 'rails_app'
+set :repo_url, 'git@github.com:gouf/rails_with_capistrano_demo.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, '/var/www/rails_app'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -33,7 +36,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
